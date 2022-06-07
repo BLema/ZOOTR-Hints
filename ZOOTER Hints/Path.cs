4017ec43aa7d60d18a4f5be1dd1e1a995e42c7fb
Brad <bradlemancik@gmail.com>
2022-06-07 10:55:08 -0500

    First

---
+++ ZOOTER Hints/Path.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZOOTR_Hints
+{
+    class Path
+    {
+        public string region { get; set; }
+        public string boss { get; set; }
+        public List<String> items;
+
+        public Path()
+        {
+            items = new List<String>();
+        }
+
+        public Path(string Region, string Boss)
+        {
+            region = Region;
+            boss = Boss;
+            items = new List<String>();
+        }
+    }
+}
